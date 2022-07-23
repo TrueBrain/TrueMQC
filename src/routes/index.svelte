f30cd4ad6f2746d0a70ba16475c3c54ae329dbb5
Patric Stout <github@truebrain.nl>
2022-07-23 19:16:30 +0000

    feat(mqtt): connect to an MQTT broker over websocket in Svelte
    This is a proof-of-work, and need actual code to be made useful.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,43 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+    import { onMount } from "svelte";
 
-	import { Tabs, Tab, TabContent } from 'carbon-components-svelte';
+    import * as mqtt from "mqtt";
 
-	function ChangeTheme(darkmode) {
-		document.documentElement.setAttribute('theme', darkmode ? 'g90' : 'g10');
-	}
+    import { Tabs, Tab, TabContent } from "carbon-components-svelte";
 
-	onMount(async () => {
-		const colorSchema = window.matchMedia('(prefers-color-scheme: dark)');
-		colorSchema.addEventListener('change', (e) => {
-			ChangeTheme(e.matches);
-		});
-		ChangeTheme(colorSchema.matches);
-	});
+    function ChangeTheme(darkmode) {
+        document.documentElement.setAttribute("theme", darkmode ? "g90" : "g10");
+    }
+
+    onMount(async () => {
+        const colorSchema = window.matchMedia("(prefers-color-scheme: dark)");
+        colorSchema.addEventListener("change", (e) => {
+            ChangeTheme(e.matches);
+        });
+        ChangeTheme(colorSchema.matches);
+
+        // Connect to localhost for testing.
+        const client = mqtt.connect("wss://127.0.0.1");
+        client.on("connect", function () {
+            console.log("Check");
+            // Subscribe to anything, just to confirm it actually works.
+            client.subscribe("#");
+        });
+        client.on("message", function (topic, message, packet) {
+            console.log(topic);
+        });
+    });
 </script>
 
 <Tabs class="topnav">
-	<Tab label="Test1" />
+    <Tab label="Test1" />
 
-	<svelte:fragment slot="content">
-		<TabContent />
-	</svelte:fragment>
+    <svelte:fragment slot="content">
+        <TabContent />
+    </svelte:fragment>
 </Tabs>
 
 <svelte:head>
-	<link rel="stylesheet" href="/all.css" />
-	<title>TrueMQC - MQTT client in your browser</title>
+    <link rel="stylesheet" href="/all.css" />
+    <title>TrueMQC - MQTT client in your browser</title>
 </svelte:head>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            // Make an import alias to the nodejs package, for easy inclusion.
+            mqtt: "mqtt/dist/mqtt.min.js",
+        },
+    },
 };
 
 export default config;
