e466a39c5120509e2d0ed00ca6f3a3f526e4da23
Patric Stout <github@truebrain.nl>
2022-07-23 18:30:38 +0000

    chore: empty Svelte project with Carbon Design System for styling

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import { Tabs, Tab, TabContent } from 'carbon-components-svelte';
+
+	function ChangeTheme(darkmode) {
+		document.documentElement.setAttribute('theme', darkmode ? 'g90' : 'g10');
+	}
+
+	onMount(async () => {
+		const colorSchema = window.matchMedia('(prefers-color-scheme: dark)');
+		colorSchema.addEventListener('change', (e) => {
+			ChangeTheme(e.matches);
+		});
+		ChangeTheme(colorSchema.matches);
+	});
+</script>
+
+<Tabs class="topnav">
+	<Tab label="Test1" />
+
+	<svelte:fragment slot="content">
+		<TabContent />
+	</svelte:fragment>
+</Tabs>
+
+<svelte:head>
+	<link rel="stylesheet" href="/all.css" />
+	<title>TrueMQC - MQTT client in your browser</title>
+</svelte:head>
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import { optimizeImports } from 'carbon-preprocess-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [optimizeImports(), preprocess()],
+
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
+	}
+};
+
+export default config;
